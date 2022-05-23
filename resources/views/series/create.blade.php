0dc43feadaa09a99b92803ee427b624aa0a9ee7f
João Passareli <ana@email.com>
2022-05-23 07:08:03 -0300

    Atualizando os arquivos

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,7 @@
 <x-layout title="Cadastrar Série">
     
-    <form>
+    <form action="/series/salvar" method="post">
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" id="nome" name="nome" class="form-control">
