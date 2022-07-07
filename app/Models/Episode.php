5149c9c111a2a378d95152ea21653b15c4e03861
Ana <ana@email.com>
2022-07-07 07:22:12 -0300

    Convertendo os valores de  dos Episodes para serem sempre enviados como booleano na api

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -3,19 +3,33 @@
 namespace App\Models;
 
 use App\Models\Season;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Episode extends Model
 {
     use HasFactory;
-    
+
     public $timestamps = false;
     protected $fillable = ['number'];
+
+    protected $casts = [
+        'watched' => 'boolean'
+    ];
 
     public function season()
     {
         $this->belongsTo(Season::class);
     }
+
+    //Pode ser feito desta forma para garantir que o $watched será booleano, mas o recomendado é usar o $casts
+    // protected function watched(): Attribute
+    // {
+    //     return new Attribute(
+    //         get: fn($watched) => (bool) $watched,
+    //         set: fn ($watched) => (bool) $watched,
+    //     );
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SeriesController;
+use App\Models\Episode;
+use App\Models\Series;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/series', SeriesController::class);
+
+//Trazer todas as temporadas de uma série específica
+Route::get('series/{series}/seasons', function (Series $series) {
+    return $series->seasons;
+});
+
+Route::get('series/{series}/episodes', function (Series $series) {
+    return $series->episodes;
+});
+
+Route::patch('/episodes/{episode}', function (Episode $episode, Request $request) {
+    $episode->watched = $request->watched;
+    $episode->save();
+
+    return $episode;
+});
